2331b89ba27d7e518df86c6c5886d94cd9e5e490
Ashkar S <ashkars178@gmail.com>
2025-10-30 09:35:32 +0530

    import-Export

---
+++ importExport/first.js
@@ -0,0 +1,7 @@
+export const name="ashkar"
+export const age=21
+
+
+// default dob="december"s
+const dob='december'
+export default dob
---
+++ importExport/second.js
@@ -0,0 +1,10 @@
+// import dob, { name } from "./first.js";
+
+//named
+
+// console.log(name)
+
+//default 
+import dob from "./first";
+
+console.log(dob)
